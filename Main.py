91514995ae86b2873a079d8bc6c5ad2477bec1de
DemonChicken1111 <77633123+DemonChicken1111@users.noreply.github.com>
2024-01-19 21:45:16 -0500

    Worked on system creation function
    Messed around with functionality of creating systems. Might rework the design entirely

--- Main.py
+++ Main.py
@@ -21,6 +21,10 @@ class EVERouter(Frame):
 		self.SysButton = Button(self, text = "New System", command = self.CreateSystem)
 		self.SysButton.grid(row = 0, column = 0, rowspan = 2)
 
+		#System Input Field
+		self.SysVar = StringVar()
+		self.SysInput = Entry(self, textvariable = self.SysVar)
+		self.SysInput.grid(row = 0, column = 3)
 
 	def CreateSystem(self):
 		#Event Handler for SysButton
@@ -30,25 +34,33 @@ class EVERouter(Frame):
 		self.SystemWidget.grid(row = 0, column = 20)
 
 		#Add Sig List
-		self.SigScroll = Scrollbar(self.SystemWidget, orient = VERTICAL)
-		self.SigList = Listbox(self.SystemWidget, width = 6, height = 10, selectmode = SINGLE, yscrollcommand = self.SigScroll.set)
-
-		if bool(self.SigList.winfo_exists()) == True:
-			print("True")
-		else:
-			self.SigScroll.grid(row = 0, column = 1, sticky = N+S)
-			self.SigList.grid(row = 0, column = 0, sticky = N+S)
-			self.SigScroll["command"] = self.SigList.yview
+		
+		self.SigList()
 
 		#Add Sig Entry Field
+
+		#self.SigScroll = Scrollbar(self.SystemWidget, orient = VERTICAL)
 		#self.SigScroll.grid(row = 0, column = 1, sticky = N+S)
 
+		#self.SigList = Listbox(self.SystemWidget, width = 6, height = 10, selectmode = SINGLE, yscrollcommand = self.SigScroll.set)
 		#self.SigList.grid(row = 0, column = 0, sticky = N+S)
 		#self.SigScroll["command"] = self.SigList.yview
 
 		#Add Sig Buttons (Add)
 
+	def SigList(self):
 
+		#System = self.SysVar.get() (Doesn't work at all lmao)
+
+		self.SigSys = Entry(self.SystemWidget, textvariable = self.SysVar) #Makes the input field update real time with entry box
+		self.SigSys.grid(row = 0, column = 0, sticky = N)
+
+		self.SigScroll = Scrollbar(self.SystemWidget, orient = VERTICAL)
+		self.SigScroll.grid(row = 1, column = 1, sticky = N+S)
+
+		self.SigList = Listbox(self.SystemWidget, width = 6, height = 10, selectmode = SINGLE, yscrollcommand = self.SigScroll.set)
+		self.SigList.grid(row = 1, column = 0, sticky = N+S)
+		self.SigScroll["command"] = self.SigList.yview
 
 def main():
 	EVERouter().mainloop()
