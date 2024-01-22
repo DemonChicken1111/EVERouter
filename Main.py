1d4b83764ca84fbe14d55cacdf978a25a98e2bac
DemonChicken1111 <77633123+DemonChicken1111@users.noreply.github.com>
2024-01-21 20:36:41 -0500

    Added Route Creation
    Added functionality to create new routes and import parsed EVE routes into Treeview children

--- Main.py
+++ Main.py
@@ -28,7 +28,7 @@ class EVERouter(Frame):
 		self.FileMenu = Menu(self.Menu, tearoff = False)
 		self.Menu.add_cascade(menu = self.FileMenu, label = "File")
 
-		self.FileMenu.add_command(label = "New Route")
+		self.FileMenu.add_command(label = "New Route", command = self.AddRoute)
 		self.FileMenu.add_command(label = "Open Route")
 		self.FileMenu.add_command(label = "Copy clipboard", command = self.AddSigs)
 
@@ -45,13 +45,15 @@ class EVERouter(Frame):
 	def AddSigs(self):
 
 		#Reads Clipboard
-		clipboard = pyperclip.paste()
+		SigClipboard = pyperclip.paste()
 
 		#Parses New Text
-		NewSigList = clipboard.split("\n")
+		NewSigList = SigClipboard.split("\n")
+			#print(NewSigList)
 
     	#Pulls SigList from save file dictonary, based on current selection ID
-		SigList = []
+    	#Current List is for testing purposes
+		SigList = ['LCX-936\tCosmic Anomaly\tOre Site\tMedium Jaspet Deposit\t100.0%\t6.17 AU\r', 'UIY-608\tCosmic Anomaly\t\t\t100.0%\t7.47 AU\r', 'HQJ-988\tCosmic Anomaly\tCombat Site\tAngel Den\t100.0%\t7.57 AU\r', 'VSG-874\tCosmic Anomaly\tCombat Site\tDrone Assembly\t100.0%\t7.82 AU\r', 'KTA-296\tCosmic Anomaly\tCombat Site\tAngel Rally Point\t100.0%\t7.97 AU\r', 'PSH-909\tCosmic Anomaly\tCombat Site\tAngel Hideaway\t100.0%\t7.97 AU\r', 'JUX-749\tCosmic Anomaly\tCombat Site\tAngel Hidden Den\t100.0%\t8.60 AU\r', 'WFM-672\tCosmic Anomaly\tCombat Site\tAngel Hideaway\t100.0%\t8.88 AU\r', 'GOU-950\tCosmic Anomaly\tOre Site\tGlacial Mass Belt\t100.0%\t8.96 AU\r', 'AYQ-296\tCosmic Anomaly\t\t        \t100.0%\t9.03 AU\r', 'EFL-300\tCosmic Signature\t\t\t0.0%\t13.51 AU\r', 'UCZ-104\tCosmic Signature\t\t\t0.0%\t49.06 AU\r', 'BAS-475\tCosmic Anomaly\tCombat Site\tAngel Rally Point\t100.0%\t49.20 AU']
 
 		#Making sure SigList is a valid input
 		if NewSigList[0][3] != "-":
@@ -71,20 +73,36 @@ class EVERouter(Frame):
 				print("existing sig found")
 				if len(n) < len(s):
 					print("replacing with more detailed sig")
-					n = s
+					n = s 
+					print(n)
+			SigList[i] = n
 					
 		#Updates SigList to now collated list
 		print("updated sigs")
-		SigList[i] = n
-		
-		#Save SigList to Route file
+		print(SigList)
 
-	def ReadData():
-		#Opens Save File
+	def AddRoute(self):
 
-		#Loads to dictionary
+		#Makes Route Name
+		Route = self.Tree.insert("", END, text = "New Route")
 
-	def WriteData():
+		#Parses Clipboard (Route)
+		RouteClipboard = pyperclip.paste()
+		RouteList = []
+	
+		#Removes "Current Location:"
+		#Looks for "(" and trims after it
+		for line in RouteClipboard.splitlines("\n"):
+			line = line.split("(")
+			if len(line) > 1:
+				RouteList.append(line[0])
+
+		print(RouteList)
+
+		#Adds Parsed Systesms as Subitems
+		for i, r in enumerate(RouteList):
+			self.Tree.insert(Route, END, text = r)
+
 
 
 def main():
